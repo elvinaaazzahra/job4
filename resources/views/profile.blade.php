34b4b4dbfdab68f18101e5a450b57c6affe295bd
elvinaaazzahra <elvina23708@gmail.com>
2025-03-10 12:37:04 +0700

    Pesan Commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="Container mt-4">
+            <div class="card">
+                <div clas="card-header">
+                    <h1>Profil Saya</h1>
+                </div>
+                <div class="card-body">
+                    <img src="images.jpeg" alt="profile icture" width="150">
+                    <p>Nama: Elvina Azzahra</p>
+                    <p>Email: elvina23708@gmail.com</p>
+                    <p>Bio: Pelajar</p>
+                    <h5>Skills:</h5>
+                    <ul>
+                        <li>HTML</li>
+                        <li>PHP</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+  </body>
+</html>
